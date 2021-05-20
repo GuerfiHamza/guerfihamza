960d77d1b26af9a59c6c4a3b54f341a6df411016
GuerfiHamza <43896427+GuerfiHamza@users.noreply.github.com>
2021-05-20 17:49:21 +0100

    main push

---
+++ public/js/client/admin/roots/478.js
@@ -0,0 +1,1 @@
+(self.webpackChunk=self.webpackChunk||[]).push([[478],{87478:(e,n,t)=>{"use strict";t.r(n),t.d(n,{default:()=>i});var u=t(67294),s=t(37703),c=t(5977),f=t(59122),a=t(97919),r=t(85893);const i=function(){var e=(0,s.I0)();return(0,u.useEffect)((function(){e((0,a.Sx)())}),[]),(0,r.jsx)(c.l_,{to:f.Z.web.admin.login})}}}]);
